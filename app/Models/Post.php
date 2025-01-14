527cd968f5b50492c70e7e4287237ae522c20098
Thoriq Dharmawan <skyshi-thoriq.darmawan@edot.id>
2025-01-14 22:02:51 +0700

    feat: add a new factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Post
+class Post extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        "id" => 1,
-        'slug' => "judul-artikel-1",
-        "title" => "Post 1",
-        "author" => "Thoriq Dharmawan",
-        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam voluptatem optioexercitationem culpa consequuntur, reprehenderit recusandae qui iure ducimus, esse molestiae? Excepturi obcaecati, animi velit laborum quibusdam tenetur fugit ratione."
-      ],
-      [
-        "id" => 2,
-        'slug' => "judul-artikel-2",
-        "title" => "Post 2",
-        "author" => "Awaludin",
-        "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam  optio exercitationem culpa consequuntur, reprehenderit recusandae qui iure ducimus, esse molestiae? Excepturi obcaecati, animi velit laborum quibusdam tenetur fugit ratione."
-      ],
-    ];
-  }
-
-  public static function find($slug): array
-  {
-    $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-    if (!$post) {
-      abort(404);
-    };
-
-    return $post;
-  }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
