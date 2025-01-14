ba5d520e9a8d5655f1561c7bd4f5e344ebe66851
thoriqdharmawan <thoriqd21@gmail.com>
2025-01-14 23:03:28 +0700

    feat: add username field

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/posts/{post:slug}', function (Post $post) {
   return view('post', ["title" => "Single Post", "post" => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
-  return view('posts', ["title" => "Articles by " . $user->name, "posts" => $user->posts]);
+Route::get('/authors/{user:username}', function (User $user) {
+  return view('posts', ["title" => '(' . count($user->posts) . ')' . " Articles by " . $user->name, "posts" => $user->posts]);
 });
 
 Route::get('/contact', function () {
