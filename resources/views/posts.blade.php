3b4990694467143d03546b1ee958ea9b69bb93bc
thoriqdharmawan <thoriqd21@gmail.com>
2025-01-15 22:45:47 +0700

    feat: Added author filter to posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,6 +7,9 @@
                 @if (request('category'))
                     <input type="hidden" name="category" value="{{ request('category') }}">
                 @endif
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
                 <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
                     <div class="relative w-full">
                         <label for="search"
