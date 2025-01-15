4e2a1713b22bdc934c525ea40cc191fa71aa71fd
thoriqdharmawan <thoriqd21@gmail.com>
2025-01-15 22:42:04 +0700

    feat: Added author filter to Post scopeFilter method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -36,5 +36,11 @@ class Post extends Model
             fn($query, $category) =>
             $query->whereHas('category', fn($query) => $query->where('slug', $category))
         );
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn($query, $author) =>
+            $query->whereHas('author', fn($query) => $query->where('username', $author))
+        );
     }
 }
